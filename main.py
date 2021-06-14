9c9f57cccd09ed3c1d69fbff18d6f7af87654c8a
weiboyuan <974531054@qq.com>
2021-06-14 12:23:17 +0800

    dense function completed

--- main.py
+++ main.py
@@ -74,31 +74,76 @@ def cluster_acc(y_true, y_pred):
     recall_micro = metrics.recall_score(y_true, new_predict, average='micro')
     return acc, f1_macro
 class AE(Module):
-    def __init__(self,n_input,n_cluster,layer=3):
+    def __init__(self,n_input,n_cluster,layer=3,dense=False):
         super(AE, self).__init__()
-        self.encs=[]
-        self.decs=[]
-        self.model=[]
-        self.h=[]
-        self.once=0
         self.num=math.ceil((n_input-n_cluster)/layer)
+        self.dense=dense
+        self.outdim=[]
+        self.layer=layer
         n_enc_in = n_input
-        n_dec_in = n_cluster
+        n_dec_in = 0
         for i in range(layer-1):
-            n_enc_in -=i*self.num
-            n_dec_in+=i * self.num
-            self.encs.append(Linear(n_input-i*self.num,n_input-(i+1)*self.num))
-            self.decs.append(Linear(n_cluster + i * self.num, n_cluster + (i + 1) * self.num))
-        self.z_layer=Linear(n_input-(layer-1)*self.num,n_cluster)
-        self.pred_layer=Linear(n_cluster+(layer-1)*self.num,n_input)
-        self.model=self.encs+[self.z_layer]+self.decs+[self.pred_layer]
-    def forward(self,X):
+            if(dense):
+                n_enc_in+=n_input-i*self.num
+                if (i == 0):
+                    self.add_module("ENC{}".format(i + 1), Linear(n_input, n_input - (i + 1) * self.num))
+                    n_enc_in = n_input
+                else:
+                    self.add_module("ENC{}".format(i + 1), Linear(n_enc_in - n_input, n_input - (i + 1) * self.num))
+            else:
+                n_enc_in -=i*self.num
+                self.add_module("ENC{}".format(i + 1),Linear(n_input - i * self.num, n_input - (i + 1) * self.num))
+            self.outdim.append(n_input - (i + 1) * self.num)
+        # construct z layer
+        if(self.dense):
+            self.add_module("Z LAYER",Linear(n_enc_in-(layer-1)*self.num,n_cluster))
+        else:
+            self.add_module("Z LAYER", Linear(n_input-(layer-1)*self.num,n_cluster))
+        self.outdim.append(n_cluster)
+        # construct decoder
+        for i in range(layer - 1):
+            if (dense):
+                n_dec_in += n_cluster + i * self.num
+                self.add_module("DEC{}".format(i + 1), Linear(n_dec_in, n_cluster + (i + 1) * self.num))
+            else:
+                n_dec_in+=i * self.num
+                self.add_module("DEC{}".format(i + 1),Linear(n_cluster + i * self.num, n_cluster + (i + 1) * self.num))
+            self.outdim.append(n_cluster + (i + 1) * self.num)
+        # construct predict layer
+        if(self.dense):
+            self.add_module("PRED LAYER",Linear(n_dec_in+n_cluster+(layer-1)*self.num,n_input))
+        else:
+            self.add_module("PRED LAYER", Linear(n_cluster+(layer-1)*self.num,n_input))
+        self.outdim.append(n_input)
+
+    def forward(self, X):
         h=X
         hz=None
-        for i in range(len(self.model)):
-           h=F.relu(self.model[i](h))
-           if(i==len(self.encs)):
-               hz=h
+        if(self.dense):
+            i=0
+            for module in self.children():
+                if(i<self.layer):
+                    if(i==0):
+                        h=F.relu(module(h))
+                    else:
+                        if(i!=self.layer-1):
+                            h=torch.cat([h,F.relu(module(h))],dim=1)
+                        else:
+                            h = F.relu(module(h))
+                            hz=h
+                else:
+                    if(i==self.layer*2-1):
+                        h=F.relu(module(h))
+                    else:
+                        h = torch.cat([h, F.relu(module(h))], dim=1)
+                i+=1
+        else:
+            i=0
+            for module in self.children():
+                h = F.relu(module(h))
+                i+=1
+                if (i == self.layer):
+                    hz = h
         return hz,h
 
 class Data_Loader:
@@ -113,7 +158,7 @@ class Data_Loader:
         ydirectory=self.directory[datasetname][1]
         Y=np.loadtxt(ydirectory,dtype=int)
         return torch.from_numpy(np.array(X)).to(torch.float32),torch.from_numpy(np.array(Y))
-def train_AE(model,x,y,epoch=600):
+def train_AE(model,x,y,epoch=100):
     ae=model
     optimizer=Adam(ae.parameters(),lr=1e-3)
     for i in range(epoch):
@@ -130,14 +175,13 @@ def train_AE(model,x,y,epoch=600):
                 pred = kmeans.fit_predict(Z.data.numpy())
                 acc,f1=cluster_acc(y.numpy(),pred)
                 print("ACC:{} F1:{}".format(acc,f1))
-    torch.save(ae.state_dict(), "AE_1.pkl")
+    torch.save(ae.state_dict(), "AE_weights.pkl")
 if(__name__=="__main__"):
     data_loader = Data_Loader()
     data_loader.load_direct(datasetname="acm", directory="./data")
     x, y = data_loader.load_data("acm")
-    model=AE(n_input=x.shape[1],n_cluster=50,layer=3)
-    for i in model.model:
-        print(i)
+    model=AE(n_input=x.shape[1],n_cluster=50,layer=5,dense=True)
+    print(model)
     train_AE(model=model,x=x,y=y)
     with torch.no_grad():
         Z, xpred = model(x)
